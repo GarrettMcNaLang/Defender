a209bbc5b9ced7fdd8d3f038eb750ea4bf589c26
GarrettMcNaLang <112025126+GarrettMcNaLang@users.noreply.github.com>
2023-02-07 17:56:11 -0500

    layout/UI/player/playercontrolls/startedshooting

---
+++ Defender/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float leftBoundary = 0f;
+    [SerializeField] float rightBoundary = 0f;
+    public float speed = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+        //checks for input
+    {   //difference between GetKey vs. GetKeyDown: GetKey returns true for every frame
+        //you have it down, while GetKeyDown will only return true for the first frame you
+        //get the key down
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.position += Vector3.left * this.speed * Time.deltaTime;
+        }
+        else if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.position += Vector3.right * this.speed * Time.deltaTime;
+        }
+
+        //creates a boundary on the left and right side of the screen
+        if(transform.position.x >= rightBoundary)
+        {
+            transform.position = new Vector3(rightBoundary, transform.position.y, 0);
+        }
+        else if(transform.position.x <= leftBoundary)
+        {
+            transform.position = new Vector3(leftBoundary,transform.position.y, 0);
+        }
+        //limiter reference from the coordinates of the x-axis will be used from the
+        //position of the object in the inspector
+    }
+}
